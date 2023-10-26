0f39e014a7fa82920b19f893655fe27f704ab533
Atro Zare'i <103967835+atrogpx@users.noreply.github.com>
2023-10-26 20:35:53 +0330

    Add files via upload

---
+++ data.py
@@ -0,0 +1,19 @@
+question_data = [
+    {"question": "The very first recorded computer &quot;bug&quot; was a moth found inside a Harvard Mark II computer.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"question": "All program codes have to be compiled into an executable file in order to be run. This file can then be executed on any machine.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"question": "&quot;Windows NT&quot; is a monolithic kernel.", "correct_answer": "False",
+     "incorrect_answers": ["True"]},
+    {"question": "To bypass US Munitions Export Laws, the creator of the PGP published all the source code in book form. ",
+     "correct_answer": "True"},
+    {"question": "The open source program Redis is a relational database server.", "correct_answer": "False"},
+    {"question": "AMD created the first consumer 64-bit processor.",
+     "correct_answer": "True"},
+    {"question": "The first dual-core CPU was the Intel Pentium D.", "correct_answer": "False"},
+    {"question": "The last Windows operating system to be based on the Windows 9x kernel was Windows 98.",
+     "correct_answer": "False"},
+    {"question": "It&#039;s not possible to format a write-protected DVD-R Hard Disk.", "correct_answer": "True"},
+    {"question": "A Boolean value of &quot;0&quot; represents which of these words?",
+     "correct_answer": "False"}
+]
